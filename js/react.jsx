ff46a9c5813c9d795e0393a8c23b7f1da7b3a20f
isaac219 <isaac219@gmail.com>
2024-01-01 13:10:17 -0500

    Overhaul to react project

--- js/react.jsx
+++
@@ -1,7 +0,0 @@
-const HelloWorld = () => {
-  return React.createElement('div', null, 'Hello world');
-};
-
-// Render the component inside the root element
-const rootElement = document.getElementById('test');
-ReactDOM.render(React.createElement(HelloWorld), rootElement);
