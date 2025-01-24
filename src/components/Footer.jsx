d7ca82b8681804562601f6a0cb57457c80b48f45
Isaac Na <isaac219@gmail.com>
2025-01-23 23:10:01 -0500

    Tweak header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import '../styles/style.css'
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <>
     <div className="footer-bar">
       <div className="row">
-        <div className="col-sm-6 footer-cell"><span className="custom-padding-left bold-font">MADELEINE FARRER</span></div>
-        <div className="col-sm-6 footer-cell"><span>STORIES TO FORM</span></div>
+        <div className="col-sm-12 footer-cell">
+            <span className="custom-padding-left bold-font">
+              <Link to='/about' className="link" style={{color: 'black'}}>
+                MADELEINE FARRER
+              </Link>
+            </span>
+          </div>
       </div>
     </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,14 @@ function Header() {
     <>
       <div className="header-bar">
         <div className="row">
-        <div className="col-sm-3 header-cell">
+        <div className="col-sm-4 header-cell">
           <Link to='/' className="custom-padding-left link" style={{color: 'white'}}>ARCHITECTURE</Link>
         </div>
-        <div className="col-sm-3 header-cell">
+        <div className="col-sm-4 header-cell">
           <Link to='/misc' className="link" style={{color: 'white'}}>MISCELLANEOUS</Link>
         </div>
-        <div className="col-sm-3 header-cell">
+        <div className="col-sm-4 header-cell">
           <Link to='https://drive.google.com/file/d/1pg4JK9asNml85M6cPAqh4KZr1PaaBM1C/view' target="_blank" className="link" style={{color: 'white'}}>PORTFOLIO</Link>
-        </div>
-        <div className="col-sm-3 header-cell">
-          <Link to='/about' className="link" style={{color: 'white'}}>WHO IS SHE</Link>
         </div>
         </div>
       </div>
