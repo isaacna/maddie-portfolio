0c50e9bd16fbcce19b38c72168ca32eb004d1769
Isaac Na <isaac219@gmail.com>
2025-01-23 23:46:51 -0500

    update about pic

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -9,10 +9,10 @@ function About() {
             <p className="about-text">I have an urge to create novel connection, belonging, and health across boundaries and scales. I believe that the future of our built systems can be written in the same language as our natural systems: resilience cultivated by local gratitude, radical positive imagination, and community wealth.</p>
             <p className="about-text">Art is how I express care for place and all forms of life. By making highly particular gifts for those I love, I give time and attention to presence and context.</p>
             <p className="about-text">Email me at mfarrer@gsd.harvard.edu.</p>
-            <p className="about-text">Check out my <a href="https://drive.google.com/file/d/1T99pyykalquLwdKH6S0UZY12CQFt2p8W/view" className="link" target="_blank">resume</a>.</p>
+            <p className="about-text">Check out my <a href="https://drive.google.com/file/d/1T99pyykalquLwdKH6S0UZY12CQFt2p8W/view" className="about-link" target="_blank">resume</a>.</p>
         </div>
         <div class="col-md-6 order-md-2">
-            <img className="about-img img-fluid rightfix" src="https://maddie-portfolio.s3.amazonaws.com/architecture/about/bio_picture.jpg" alt="Your Image" />
+            <img className="about-img img-fluid rightfix" src="https://maddie-portfolio.s3.amazonaws.com/about/MF_Headshot_Color_08.jpg" alt="Your Image" />
         </div>
     </div>
   );
